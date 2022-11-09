467b20103ead07c84d1fb8c0a553e21689d79495
Fauza Riawan <umardastan2014@gmail.com>
2022-11-09 14:47:10 +0700

    add redis

--- services/api.js
+++ services/api.js
@@ -1,31 +1,43 @@
 const axios = require('axios')
-
+const Redis = require('redis')
+const redisClient = Redis.createClient()
+const DEFAULT_EXP = 3600
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 class API {
   static async pokemons(){
     try {
-      let result = await axios({
-        method: 'get',
-        url: 'https://pokeapi.co/api/v2/pokemon',
-        headers: {
-          'Content-Type': 'text/plain',
+      await redisClient.connect()
+      const pokemons = await redisClient.get('pokemons');
+      if(pokemons == null ){
+        let result = await axios({
+          method: 'get',
+          url: 'https://pokeapi.co/api/v2/pokemon',
+          headers: {
+            'Content-Type': 'text/plain',
+          }
+        })
+        let status = result.status
+        let headers = JSON.parse(JSON.stringify(result.headers))
+        let response = JSON.parse(JSON.stringify(result.data))
+        if(status == 200){
+          let pokeDetail = response.results
+          for (let index = 0; index < pokeDetail.length; index++) {
+              let result = await axios({
+                method: 'get',
+                url: pokeDetail[index].url,
+                headers: {
+                  'Content-Type': 'text/plain',
+                }
+              })
+              pokeDetail[index].image = result.data.sprites.front_default
+          }
+          await redisClient.setEx('pokemons', DEFAULT_EXP, JSON.stringify(pokeDetail))
+          await redisClient.disconnect();
+          return pokeDetail
         }
-      })
-      let status = result.status
-      let headers = JSON.parse(JSON.stringify(result.headers))
-      let response = JSON.parse(JSON.stringify(result.data))
-      if(status == 200){
-        let pokeDetail = response.results
-        for (let index = 0; index < pokeDetail.length; index++) {
-            let result = await axios({
-              method: 'get',
-              url: pokeDetail[index].url,
-              headers: {
-                'Content-Type': 'text/plain',
-              }
-            })
-            pokeDetail[index].image = result.data.sprites.front_default
-        }
-        return pokeDetail
+      }else{
+        await redisClient.disconnect();
+        return JSON.parse(pokemons)
       }
       return null
     } catch (error) {
